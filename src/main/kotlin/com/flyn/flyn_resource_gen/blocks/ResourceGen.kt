370a3361161afa52bad0fa112a8fb060b6a5201f
EternalFlyn <EternalFlyn@gmail.com>
2024-12-10 14:11:44 +0800

    2024-12-10 setting the property and the model of resource gen block

--- src/main/kotlin/com/flyn/flyn_resource_gen/blocks/ResourceGen.kt
+++ src/main/kotlin/com/flyn/flyn_resource_gen/blocks/ResourceGen.kt
@@ -4,6 +4,7 @@ import com.flyn.flyn_resource_gen.FlynResourceGen
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.material.MapColor
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
@@ -12,12 +13,13 @@ import thedarkcolour.kotlinforforge.forge.registerObject
 @EventBusSubscriber(modid = FlynResourceGen.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 object ResourceGen {
 
-    const val BLOCK_NAME = "resource_gen"
+    const val BLOCK_NAME = "resource_gen_block"
 
     val resourceGenBlock by FlynResourceGen.BLOCKS.registerObject(BLOCK_NAME) {
         Block(
-            BlockBehaviour.Properties.of().apply {
+            BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).apply {
                 mapColor(MapColor.STONE)
+                lightLevel { 10 }
             }
         )
     }
