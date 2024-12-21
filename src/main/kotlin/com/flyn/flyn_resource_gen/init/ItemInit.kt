b9dcb955cff53ae8c823bc513275ff196aab6f5f
EternalFlyn <EternalFlyn@gmail.com>
2024-12-21 13:04:30 +0800

    2024-12-21 add all the resource variant to creative tab

--- src/main/kotlin/com/flyn/flyn_resource_gen/init/ItemInit.kt
+++ src/main/kotlin/com/flyn/flyn_resource_gen/init/ItemInit.kt
@@ -1,13 +1,13 @@
 package com.flyn.flyn_resource_gen.init
 
 import com.flyn.flyn_resource_gen.blocks.ResourceGenBlock
-import com.flyn.flyn_resource_gen.init.TabInit.registerToMainTab
 import com.flyn.flyn_resource_gen.items.ResourceGenItem
 import net.minecraft.world.item.Item
 import net.minecraftforge.registries.ForgeRegistries
+import thedarkcolour.kotlinforforge.forge.registerObject
 
 object ItemInit : Initializer<Item>(ForgeRegistries.ITEMS) {
 
-    val RESOURCE_GEN_BLOCK_ITEM by register.registerToMainTab(ResourceGenBlock.BLOCK_NAME) { ResourceGenItem() }
+    val RESOURCE_GEN_BLOCK_ITEM by register.registerObject(ResourceGenBlock.BLOCK_NAME) { ResourceGenItem() }
 
 }
--- src/main/kotlin/com/flyn/flyn_resource_gen/init/TabInit.kt
+++ src/main/kotlin/com/flyn/flyn_resource_gen/init/TabInit.kt
@@ -1,25 +1,19 @@
 package com.flyn.flyn_resource_gen.init
 
+import com.flyn.flyn_resource_gen.Config
+import com.flyn.flyn_resource_gen.misc.ResourceGenNbt
+import com.flyn.flyn_resource_gen.misc.put
+import com.flyn.flyn_resource_gen.misc.thisModTag
+import com.flyn.flyn_resource_gen.setTag
 import net.minecraft.core.registries.Registries
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.chat.Component
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.CreativeModeTabs
-import net.minecraft.world.item.Item
-import net.minecraftforge.registries.DeferredRegister
-import thedarkcolour.kotlinforforge.forge.ObjectHolderDelegate
+import net.minecraft.world.item.ItemStack
 import thedarkcolour.kotlinforforge.forge.registerObject
-import java.util.function.Supplier
 
 object TabInit : Initializer<CreativeModeTab>(Registries.CREATIVE_MODE_TAB) {
-
-    private val mainTabItems = mutableListOf<Supplier<out Item>>()
-
-    fun <T: Item, U: T> DeferredRegister<T>.registerToMainTab(
-        name: String,
-        supplier: () -> U
-    ): ObjectHolderDelegate<U> = this.registerObject(name, supplier).also {
-        mainTabItems.add(it.registryObject)
-    }
 
     @Suppress("UNUSED")
     val MAIN_TAB by register.registerObject("main_tab") {
@@ -30,7 +24,19 @@ object TabInit : Initializer<CreativeModeTab>(Registries.CREATIVE_MODE_TAB) {
                 ItemInit.RESOURCE_GEN_BLOCK_ITEM.defaultInstance
             }
             displayItems { _, output ->
-                mainTabItems.forEach { output.accept(it.get()) }
+                val items = mutableListOf<ItemStack>()
+                for (i in 1..5) {
+                    items += Config.canGenerateBlocks.keys.map {
+                        ItemStack(ItemInit.RESOURCE_GEN_BLOCK_ITEM, 1).setTag {
+                            val data = CompoundTag().apply {
+                                put(ResourceGenNbt.Tier, i)
+                                put(ResourceGenNbt.Product, it)
+                            }
+                            thisModTag = data
+                        }
+                    }
+                }
+                output.acceptAll(items)
             }
         }.build()
     }
