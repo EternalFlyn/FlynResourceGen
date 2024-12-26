f43c6399caee07aed1681dcf9f39f2fd7d9d2909
EternalFlyn <EternalFlyn@gmail.com>
2024-12-27 00:31:58 +0800

    2024-12-26 custom the model of the resource gen block

--- src/main/kotlin/com/flyn/flyn_resource_gen/Utils.kt
+++ src/main/kotlin/com/flyn/flyn_resource_gen/Utils.kt
@@ -1,13 +1,16 @@
 package com.flyn.flyn_resource_gen
 
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
+import com.google.common.collect.ImmutableMap
+import net.minecraft.client.renderer.block.model.ItemTransform
+import net.minecraft.client.renderer.block.model.ItemTransforms
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemDisplayContext
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import net.minecraftforge.registries.ForgeRegistries
+import org.joml.Vector3f
 
 fun CompoundTag.getItemId(tag: String): Item = getString(tag).let {
     val resource = ResourceLocation(it)
@@ -23,17 +26,69 @@ fun CompoundTag.putItemId(tag: String, item: Item) {
     putString(tag, str)
 }
 
-fun JsonObject.addArray(property: String, array: JsonArray.() -> Unit) {
-    this.add(property, JsonArray().apply { array() })
-}
-
-fun JsonObject.addObject(property: String, obj: JsonObject.() -> Unit) {
-    this.add(property, JsonObject().apply { obj() })
-}
-
 fun ItemStack.setTag(nbt: CompoundTag.() -> Unit): ItemStack {
     if (tag == null)
         tag = CompoundTag()
     nbt(tag!!)
     return this
 }
+
+val DEFAULT_ITEM_TRANSFORMS = object : ItemTransforms(
+    // ThirdPersonLeftHand
+    ItemTransform(
+        Vector3f(75f, 45f, 0f),
+        Vector3f(0f, 2.5f / 16f, 0f),
+        Vector3f(0.375f, 0.375f, 0.375f),
+        Vector3f(0.0f, 0.0f, 0.0f)
+    ),
+    // ThirdPersonRightHand
+    ItemTransform(
+        Vector3f(75f, 45f, 0f),
+        Vector3f(0f, 2.5f / 16f, 0f),
+        Vector3f(0.375f, 0.375f, 0.375f),
+        Vector3f(0.0f, 0.0f, 0.0f)
+    ),
+    // FirstPersonLeftHand
+    ItemTransform(
+        Vector3f(0f, 225f, 0f),
+        Vector3f(0f, 0f, 0f),
+        Vector3f(0.4f, 0.4f, 0.4f),
+        Vector3f(0.0f, 0.0f, 0.0f)
+    ),
+    // FirstPersonRightHand
+    ItemTransform(
+        Vector3f(0f, 45f, 0f),
+        Vector3f(0f, 0f, 0f),
+        Vector3f(0.4f, 0.4f, 0.4f),
+        Vector3f(0.0f, 0.0f, 0.0f)
+    ),
+    // Head
+    ItemTransform(
+        Vector3f(0f, 0f, 0f),
+        Vector3f(0f, 14.25f / 16f, 0f),
+        Vector3f(1f, 1f, 1f),
+        Vector3f(0.0f, 0.0f, 0.0f)
+    ),
+    // Gui
+    ItemTransform(
+        Vector3f(30f, 225f, 0f),
+        Vector3f(0f, 0f, 0f),
+        Vector3f(0.625f, 0.625f, 0.625f),
+        Vector3f(0.0f, 0.0f, 0.0f)
+    ),
+    // Ground
+    ItemTransform(
+        Vector3f(0f, 0f, 0f),
+        Vector3f(0f, 3 / 16f, 0f),
+        Vector3f(0.25f, 0.25f, 0.25f),
+        Vector3f(0.0f, 0.0f, 0.0f)
+    ),
+    // Fixed
+    ItemTransform(
+        Vector3f(0f, 0f, 0f),
+        Vector3f(0f, 0f, 0f),
+        Vector3f(0.5f, 0.5f, 0.5f),
+        Vector3f(0.0f, 0.0f, 0.0f)
+    ),
+    ImmutableMap.builder<ItemDisplayContext, ItemTransform>().build()
+) {}
